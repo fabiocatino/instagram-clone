d0ccfaed7a0044e3f43ef98a46c15422733f94aa
Fabio Catino <fabivs9@gmail.com>
2022-10-15 08:28:17 +0200

    faker library replaced

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import faker from 'faker';
-import Story from './Story';
+import React, { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import Story from "./Story";
 
 const Stories = () => {
-	const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
-	useEffect(() => {
-		const suggestions = [...Array(20)].map((_, i) => ({
-			...faker.helpers.contextualCard(),
-			id: i,
-		}));
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, index) => ({
+      username: faker.internet.userName(),
+      avatar: faker.image.people(500, 500, true),
+      id: index,
+    }));
 
-		setSuggestions(suggestions);
-	}, []);
+    setSuggestions(suggestions);
+  }, []);
 
-	return (
-		<div
-			className="flex overflow-x-scroll scrollbar-thin scrollbar-thumb-black space-x-2 p-4
+  return (
+    <div
+      className="flex overflow-x-scroll scrollbar-thin scrollbar-thumb-black space-x-2 p-4
             bg-white border-gray-20 border rounded-sm h-[118px]"
-		>
-			{suggestions.map((profile) => (
-				<Story
-					key={profile.id}
-					img={profile.avatar}
-					username={profile.username}
-				/>
-			))}
-		</div>
-	);
+    >
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Stories;
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,49 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import faker from 'faker';
+import React, { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 const Suggestions = () => {
-	const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
-	useEffect(() => {
-		const suggestions = [...Array(5)].map((item, index) => ({
-			...faker.helpers.contextualCard(),
-			id: index,
-		}));
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, index) => ({
+      name: faker.name.fullName(),
+      avatar: faker.image.people(500, 500, true),
+      company: faker.company.name().split(", ").slice(0, 10),
+      id: index,
+    }));
 
-		setSuggestions(suggestions);
-	}, []);
+    setSuggestions(suggestions);
+  }, []);
+  return (
+    <div className="space-y-4">
+      <div className="flex justify-between text-sm mb-5">
+        <p className="text-sm text-gray-500 font-semibold">
+          Suggestions for you
+        </p>
+        <button className="text-xs font-semibold">See all</button>
+      </div>
 
-	return (
-		<div className="space-y-4">
-			<div className="flex justify-between text-sm mb-5">
-				<p className="text-sm text-gray-500 font-semibold">
-					Suggestions for you
-				</p>
-				<button className="text-xs font-semibold">See all</button>
-			</div>
+      {suggestions.map((user) => (
+        <div
+          key={user.id}
+          className="flex items-center justify-between gap-2 mt-3"
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="h-8 w-8 object-contain rounded-full border p-[2px]"
+            src={user.avatar}
+            alt={user.name}
+          />
+          <div className="flex-1 ml-0.5">
+            <p className="text-sm font-semibold">{user.name}</p>
+            <p className="text-xs text-gray-400 truncate">
+              Works at {user.company}
+            </p>
+          </div>
 
-			{suggestions.map((user) => (
-				<div
-					key={user.id}
-					className="flex items-center justify-between gap-2 mt-3"
-				>
-					<img
-						className="h-8 w-8 object-contain rounded-full border p-[2px]"
-						src={user.avatar}
-						alt={user.name}
-					/>
-                    <div className='flex-1 ml-0.5'>
-					<p className="text-sm font-semibold">{user.name}</p>
-                    <p  className="text-xs text-gray-400 truncate">Works at {user.company.name}</p>
-                    </div>
-
-					<button className="text-xs font-semibold text-blue-500">
-						Follow
-					</button>
-				</div>
-			))}
-		</div>
-	);
+          <button className="text-xs font-semibold text-blue-500">
+            Follow
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Suggestions;
